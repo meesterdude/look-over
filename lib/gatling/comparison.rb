cc717e7c3fb5735f4d05f19c7b989afd4b92f71f
meesterdude <violentpurr@gmail.com>
2013-08-05 23:43:25 -0400

    Update comparison.rb

--- lib/gatling/comparison.rb
+++ lib/gatling/comparison.rb
@@ -21,7 +21,9 @@ module Gatling
 
     def compare_images_with_same_size
       images_to_compare = prep_images_for_comparison
-      images_to_compare.first.compare_channel(images_to_compare.last, Magick::MeanAbsoluteErrorMetric)
+      images_to_compare.first.compare_channel(images_to_compare.last, Magick::MeanAbsoluteErrorMetric) do
+        self.highlight_color = Pixel.new(2,200,100,.5)
+      end
     end
 
     def compare_images_with_different_size
