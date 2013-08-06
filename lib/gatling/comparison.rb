cef579d54b86a97c96dea5a383713cb495819a30
meesterdude <violentpurr@gmail.com>
2013-08-06 00:05:19 -0400

    Update comparison.rb

--- lib/gatling/comparison.rb
+++ lib/gatling/comparison.rb
@@ -36,8 +36,8 @@ module Gatling
         expanded_image
       end
     images_to_compare.first.compare_channel(images_to_compare.last, Magick::MeanAbsoluteErrorMetric) do
-        self.highlight_color = Magick::Pixel.new(65300,100,100,30)
-        self.lowlight_color = Magick::Pixel.new(100,65300,100,30)
+        self.highlight_color = Magick::Pixel.new(65300,100,100,3000)
+        self.lowlight_color = Magick::Pixel.new(100,65300,100,3000)
       end
     end
 
